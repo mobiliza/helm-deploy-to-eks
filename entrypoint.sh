c82b74b1de0b283d5b19c5030ecfe6de813edc6e
Tiago Peixoto <tiago.peixoto@mobiliza.com.br>
2020-02-26 13:30:25 -0300

    Adds more options to API

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ echo "Will deploy to EKS"
 export KUBECONFIG=/root/.kube/config
 aws eks --region $CLUSTER_REGION update-kubeconfig --name $CLUSTER_NAME
 
-echo "Deploying version $VERSION to $ENVIRONMENT environment"
+echo "Deploying version to $ENVIRONMENT environment with:"
 
-helm -n $ENVIRONMENT --set ci.version=$VERSION upgrade --install $PROJECT ./chart
+setValues=$(printf " --set %s" $SET_VALUES)
+echo "helm -n $ENVIRONMENT upgrade --install $PROJECT .helm --reuse-values $setValues"
+eval "helm -n $ENVIRONMENT upgrade --install $PROJECT .helm --reuse-values $setValues"
