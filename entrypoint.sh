e8a201c287cc08770b824e3000377a35a1bbab80
Tiago Peixoto <tiago.peixoto@mobiliza.com.br>
2020-02-26 14:54:03 -0300

    Uses convenient env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,26 @@
-#!/bin/bash -l
+#!/bin/sh -l
 
-# $1: {{ inputs.eks-cluster-accesskey }}
-# $2: {{ secrets.eks-cluster-secretkey }}
-# $3: {{ inputs.eks-cluster-region }}
-# $4: {{ inputs.eks-cluster-name }}
-# $5: {{ inputs.namespace }}
-# $6: {{ inputs.helm-release }}
-# $7: {{ inputs.set-values }}
+# $1: inputs.eks_cluster_accesskey
+# $2: inputs.eks_cluster_secretkey
+# $3: inputs.eks_cluster_region
+# $4: inputs.eks_cluster_name
+# $5: inputs.namespace
+# $6: inputs.helm_release
+# $7: inputs.set_values
 
-echo "Will deploy to EKS"
+echo "Checking ENV"
 
-AWS_ACCESS_KEY_ID=$1
-AWS_SECRET_ACCESS_KEY=$2
+printenv
 
+echo "Coonects to EKS"
+
+export AWS_ACCESS_KEY_ID=$INPUT_EKS_CLUSTER_ACCESSKEY
+export AWS_SECRET_ACCESS_KEY=$INPUT_EKS_CLUSTER_ACCESSKEY
 export KUBECONFIG=/root/.kube/config
-aws eks --region $3 update-kubeconfig --name $4
+aws eks --region $INPUT_CLUSTER_REGION update-kubeconfig --name $INPUT_CLUSTER_NAME
 
-echo "Deploying version to $5 environment with:"
+echo "Deploying version to $INPUT_NAMESPACE environment with:"
 
-setValues=$(printf " --set %s" $7)
-echo "helm -n $5 upgrade --install $6 .helm --reuse-values $setValues"
-eval "helm -n $5 upgrade --install $6 .helm --reuse-values $setValues"
+setValues=$(printf " --set %s" $INPUT_SET_VALUES)
+echo "helm -n $INPUT_NAMESPACE upgrade --install $INPUT_HELM_RELEASE .helm --reuse-values $setValues"
+eval "helm -n $INPUT_NAMESPACE upgrade --install $INPUT_HELM_RELEASE .helm --reuse-values $setValues"
