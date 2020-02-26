43373119e259ff42d77c552997ea42e3f739eeff
Tiago Peixoto <tiago.peixoto@mobiliza.com.br>
2020-02-26 14:26:25 -0300

    Use args instead of env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -l
 
+# $1: {{ inputs.eks-cluster-accesskey }}
+# $2: {{ secrets.eks-cluster-secretkey }}
+# $3: {{ inputs.eks-cluster-region }}
+# $4: {{ inputs.eks-cluster-name }}
+# $5: {{ inputs.namespace }}
+# $6: {{ inputs.helm-release }}
+# $7: {{ inputs.set-values }}
+
 echo "Will deploy to EKS"
 
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+
 export KUBECONFIG=/root/.kube/config
-aws eks --region $INPUT_EKS-CLUSTER-REGION  update-kubeconfig --name $INPUT_EKS-CLUSTER-NAME 
+aws eks --region $3 update-kubeconfig --name $4
 
-echo "Deploying version to $ENVIRONMENT environment with:"
+echo "Deploying version to $5 environment with:"
 
-setValues=$(printf " --set %s" $SET_VALUES)
-echo "helm -n $ENVIRONMENT upgrade --install $PROJECT .helm --reuse-values $setValues"
-eval "helm -n $ENVIRONMENT upgrade --install $PROJECT .helm --reuse-values $setValues"
+setValues=$(printf " --set %s" $7)
+echo "helm -n $5 upgrade --install $6 .helm --reuse-values $setValues"
+eval "helm -n $5 upgrade --install $6 .helm --reuse-values $setValues"
