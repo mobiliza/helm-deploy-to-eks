3666bffe639d858d89075c235d1a70a9864c98cd
Tiago Peixoto <tiago.peixoto@mobiliza.com.br>
2020-02-26 15:13:30 -0300

    Adds logging of helm values

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,16 @@ echo "Connects to EKS"
 export KUBECONFIG=/root/.kube/config
 aws eks --region $CLUSTER_REGION update-kubeconfig --name $CLUSTER_NAME
 
+echo "Fetch current helm values.yaml"
+
+helm -n $INPUT_NAMESPACE get values $INPUT_HELM_RELEASE > previousValues.yaml
+cat previousValues.yaml
+
 echo "Deploying version to $INPUT_NAMESPACE environment with:"
 
 setValues=$(printf " --set %s" $INPUT_SET_VALUES)
-echo "helm -n $INPUT_NAMESPACE upgrade --install $INPUT_HELM_RELEASE .helm --reuse-values $setValues"
-eval "helm -n $INPUT_NAMESPACE upgrade --install $INPUT_HELM_RELEASE .helm --reuse-values $setValues"
+echo "helm -n $INPUT_NAMESPACE upgrade --install $INPUT_HELM_RELEASE .helm -f previousValues.yaml $setValues"
+eval "helm -n $INPUT_NAMESPACE upgrade --install $INPUT_HELM_RELEASE .helm -f previousValues.yaml $setValues"
+
+echo "Check new helm values.yaml"
+helm -n $INPUT_NAMESPACE get values $INPUT_HELM_RELEASE
